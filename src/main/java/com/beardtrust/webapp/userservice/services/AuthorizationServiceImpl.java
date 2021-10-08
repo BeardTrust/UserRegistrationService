d71b151bc73efacd38d26dd20533c44317bde966
Nathanael Grier <nathanael.grier@smoothstack.com>
2021-10-08 11:28:27 -0500

    Authorization logging

--- src/main/java/com/beardtrust/webapp/userservice/services/AuthorizationServiceImpl.java
+++ src/main/java/com/beardtrust/webapp/userservice/services/AuthorizationServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * The type Authorization service.
@@ -16,6 +17,7 @@ import java.util.Optional;
  * @author Matthew Crowell <Matthew.Crowell@Smoothstack.com>
  */
 @Service
+@Slf4j
 public class AuthorizationServiceImpl implements AuthorizationService {
 	private final AuthorizationRepository authorizationRepository;
 
@@ -26,12 +28,15 @@ public class AuthorizationServiceImpl implements AuthorizationService {
 	 */
 	@Autowired
 	public AuthorizationServiceImpl(AuthorizationRepository authorizationRepository) {
+            log.trace("Creating Authorization Service...");
 		this.authorizationRepository = authorizationRepository;
 	}
 
 
 	@Override
 	public UserDTO getUserByUserId(String id) {
+            log.trace("Authorization searching for user by Id...");
+            log.debug("Authorization searching by Id: " + id);
 		Optional<UserEntity> user = authorizationRepository.findById(id);
 
 		UserDTO userDTO = null;
@@ -40,9 +45,12 @@ public class AuthorizationServiceImpl implements AuthorizationService {
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
 		if (user.isPresent()) {
+                    log.trace("User found, creating model...");
 			userDTO = modelMapper.map(user.get(), UserDTO.class);
+                        log.debug("Model created: " + userDTO);
 		}
 
+                log.trace("Authorization finished. Returning DTO...");
 		return userDTO;
 	}
 }
